4ab789ff6cac716e67200fff1344f2b927b64988
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-15 14:37:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import axios from "axios";
 
+
 const app = express();
 app.use(express.json());
+
+const ACCESS_TOKEN = "4c1af41ab7f469419a76eee8d6930a31485d7af0";
 
 app.get("/callback", async (req, res) => {
   try {
@@ -95,4 +98,30 @@ app.get("/gerar-token", async (req, res) => {
 // ✅ SERVIDOR
 app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor rodando");
+});
+// RefresToken
+app.get("/refresh-token", async (req, res) => {
+  try {
+    const params = new URLSearchParams();
+    params.append("grant_type", "refresh_token");
+    params.append("refresh_token", "26d97a62308b5dd91105282a6a134a8513bd9a4c");
+    params.append("client_id", "3ce0ca5a754902d36bd3c27fd0be1f49f0790b3c");
+    params.append("client_secret", "SEU_CLIENT_SECRET");
+
+    const response = await axios.post(
+      "https://developer.bling.com.br/api/bling/oauth/token",
+      params,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
+    );
+
+    res.json(response.data);
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.json({ erro: "falha ao atualizar token" });
+  }
 });
