6eede82f54b0ef3b28b44c1d89cbcd7776139053
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-15 19:48:40 -0300

    Create regras_ml_matriz.js

---
+++ regras_ml_matriz.js
@@ -0,0 +1,18 @@
+const REGRAS = [
+  {
+    nome: "PASSALACQUA",
+    lojaId: 204560827,
+    unidades: [2557723],
+    statusOrigem: 6,
+    statusDestino: 462966
+  },
+  {
+    nome: "FORNECEDOR 2",
+    lojaId: 204560827,
+    unidades: [2532043, 2803281],
+    statusOrigem: 6,
+    statusDestino: 462097
+  }
+];
+
+export default REGRAS;
