0c67324ca8f1ed5a90618cfb77461b95c3e81632
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-15 10:29:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
+app.get("/callback", (req, res) => {
+  const code = req.query.code;
+
+  console.log("CODE RECEBIDO:", code);
+
+  res.send(`Code recebido: ${code}`);
+});
+
 const ACCESS_TOKEN = "SEU_ACCESS_TOKEN_AQUI";
 
 // ✅ SEU TESTE DO BLING
@@ -31,14 +39,17 @@ app.get("/teste-bling", async (req, res) => {
 // 🔥 COLOCA AQUI (NOVO ENDPOINT)
 app.get("/gerar-token", async (req, res) => {
   try {
+    const code = req.query.code;
+
     const params = new URLSearchParams();
-    params.append("grant_type", "refresh_token");
-    params.append("refresh_token", "SEU_REFRESH_TOKEN");
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "https://bling-webhook.onrender.com/callback");
     params.append("client_id", "3ce0ca5a754902d36bd3c27fd0be1f49f0790b3c");
     params.append("client_secret", "105e48387b6fb4a2398566768cd529d9a9df30c78ad4161df0454e00879d");
 
     const response = await axios.post(
-      "https://www.bling.com.br/Api/v3/oauth/token",
+      "https://developer.bling.com.br/api/bling/oauth/token",
       params,
       {
         headers: {
@@ -46,9 +57,6 @@ app.get("/gerar-token", async (req, res) => {
         }
       }
     );
-
-    console.log("NOVO TOKEN:");
-    console.log(response.data);
 
     res.json(response.data);
 
