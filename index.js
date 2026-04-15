56109e0284c95efb1d1888c96ebc2b63d06f7aa7
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-15 14:39:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -64,37 +64,6 @@ app.get("/teste-bling", async (req, res) => {
   }
 });
 
-
-// 🔥 COLOCA AQUI (NOVO ENDPOINT)
-app.get("/gerar-token", async (req, res) => {
-  try {
-    const code = req.query.code;
-
-    const params = new URLSearchParams();
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", "https://bling-webhook.onrender.com/callback");
-    params.append("client_id", "3ce0ca5a754902d36bd3c27fd0be1f49f0790b3c");
-    params.append("client_secret", "105e48387b6fb4a2398566768cd529d9a9df30c78ad4161df0454e00879d");
-
-    const response = await axios.post(
-      "https://developer.bling.com.br/api/bling/oauth/token",
-      params,
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }
-    );
-
-    res.json(response.data);
-
-  } catch (error) {
-    console.error(error.response?.data || error.message);
-    res.json({ erro: "falha ao gerar token" });
-  }
-});
-
 // ✅ SERVIDOR
 app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor rodando");
