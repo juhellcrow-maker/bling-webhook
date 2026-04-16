f517b67ec8a7f0808b52a75e76832fbc18aca62f
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-16 18:29:35 -0300

    Comment out example rule in regras_ml_matriz.js
    Comment out a block of code for future reference.

--- regras_ml_matriz.js
+++ regras_ml_matriz.js
@@ -13,6 +13,14 @@ const REGRAS = [
     statusOrigem: 6,
     statusDestino: 462097
   }
+ /*{
+    nome: "SERV-SEG CATANDUVA",
+    lojaId: 204560827,
+    unidades: [2532043, 2803281],
+    statusOrigem: 6,
+    statusDestino: 462097
+  }*/
+  
 ];
 
 export default REGRAS;
