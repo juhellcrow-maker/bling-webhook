a6d53cbf53688df06276c29c2ddbb53455ed9e38
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-17 10:24:48 -0300

    Update regras_ml_matriz.js

--- regras_ml_matriz.js
+++ regras_ml_matriz.js
@@ -27,19 +27,7 @@ const REGRAS_ML_MATRIZ = [
 ];
 
 export default REGRAS_ML_MATRIZ;
- /*{
-    nome: "SERV-SEG CATANDUVA",
-    lojaId: 204560827,
-    unidades: [2532043, 2803281],
-    statusOrigem: 6,
-    statusDestino: 462097
-  }*/
-  
-];
-//status: 741645 - Passalaca Franca
-//status: 462098 - Passalacqua São Carlos
-//status: 462966 - Passalacqua Riberião
-//Status: 462967 - Envio Programado
+
 //Status: 462099 - Serv-Seg Catanduva
 //Status: 462097 - Serv-Seg Rio Preto
 
