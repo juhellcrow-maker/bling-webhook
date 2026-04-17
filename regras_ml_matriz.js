b9a9a68f4de89d6cbdbf0ba91839a62e66e64cbc
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-17 10:08:34 -0300

    Refactor REGRAS to REGRAS_ML_MATRIZ with updates

--- regras_ml_matriz.js
+++ regras_ml_matriz.js
@@ -1,18 +1,32 @@
-const REGRAS = [
+// regras_ml_matriz.js
+
+const REGRAS_ML_MATRIZ = [
   {
-    nome: "PASSALACQUA",
+    nome: "ML MATRIZ - PASSALACQUA RIBEIRÃO",
     lojaId: 204560827,
-    unidades: [2557723],
+    unidadeNegocioId: 2557723, // Passalacqua Ribeirão
     statusOrigem: 6,
     statusDestino: 462966
   },
+
   {
-    nome: "SERV-SEG RIO PRETO",
+    nome: "ML MATRIZ - SERV-SEG RIO PRETO",
     lojaId: 204560827,
-    unidades: [2532043, 2803281],
+    unidadeNegocioId: 2532043, // Serv-Seg Rio Preto
+    statusOrigem: 6,
+    statusDestino: 462097
+  },
+
+  {
+    nome: "ML MATRIZ - SERV-SEG RIO PRETO (UNIDADE 2)",
+    lojaId: 204560827,
+    unidadeNegocioId: 2803281, // Serv-Seg Rio Preto (outra unidade)
     statusOrigem: 6,
     statusDestino: 462097
   }
+];
+
+export default REGRAS_ML_MATRIZ;
  /*{
     nome: "SERV-SEG CATANDUVA",
     lojaId: 204560827,
@@ -28,4 +42,4 @@ const REGRAS = [
 //Status: 462967 - Envio Programado
 //Status: 462099 - Serv-Seg Catanduva
 //Status: 462097 - Serv-Seg Rio Preto
-export default REGRAS;
+
