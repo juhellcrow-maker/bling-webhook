50da8be61cb6d0ac84ca13a8a0450356356cfa25
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-26 15:55:35 -0300

    Refactor database connection to use DATABASE_URL

--- db.js
+++ db.js
@@ -2,12 +2,6 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: process.env.SUPABASE_DB_HOST,
-  port: Number(process.env.SUPABASE_DB_PORT || 5432),
-  database: process.env.SUPABASE_DB_NAME,
-  user: process.env.SUPABASE_DB_USER,
-  password: process.env.SUPABASE_DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
