43270c730510e3435504549a3700a7ae3ef99e48
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-26 12:02:13 -0300

    Update database connection settings for Supabase

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.SUPABASE_DB_HOST,
+  port: 5432,
+  user: process.env.SUPABASE_DB_USER,
+  password: process.env.SUPABASE_DB_PASSWORD,
+  database: process.env.SUPABASE_DB_NAME,
   ssl: { rejectUnauthorized: false }
 });
