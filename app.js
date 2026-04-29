8079ede6a45af3c681f0f7a620e8433ec6f63701
juhellcrow-maker <ju.hellcrow@gmail.com>
2026-04-29 20:21:08 -0300

    Add new routes for WhatsApp and OAuth

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ import express from "express";
 import depositosRoutes from "./routes/depositos.routes.js";
 import debugRoutes from "./routes/debug.routes.js";
 import webhookRoutes from "./routes/webhook.routes.js";
+import whatsappRoutes from "./routes/whatsapp.routes.js";
 import healthRoutes from "./routes/health.routes.js";
+import oauthRoutes from "./routes/oauth.routes.js";
+
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,8 @@ app.use(express.json());
 app.use(depositosRoutes);
 app.use(debugRoutes);
 app.use(webhookRoutes);
+app.use(whatsappRoutes);
 app.use(healthRoutes);
+app.use(oauthRoutes);
 
 export default app;
